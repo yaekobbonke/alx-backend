cb5b766f34984486266bd5e4e8e2f30913bc605d
yaekobbonke <yaekobbundure@gmail.com>
2023-10-22 20:04:17 +0300

    simple helper function

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+index_range functtion
+"""
+
+
+def index_range(page, page_size):
+    """return a tuple of size two 
+    containing a start index and an end index"""
+    start_index = (page - 1) * page_size
+    end_index = start_index+page_size
+    return start_index, end_index
