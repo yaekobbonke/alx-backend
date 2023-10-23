a228d04dd3be047463f038ffc6164542047ca53f
yaekobbonke <yaekobbundure@gmail.com>
2023-10-23 20:39:15 +0300

    delete pagination

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -40,6 +40,7 @@ class Server:
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """get gyper index"""
         assert index is None or (
             isinstance(
                 index, int) and index >= 0), "Invalid index value"
