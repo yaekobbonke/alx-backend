0d5cd46eb111cf25a3e794b4ee0f1a699a832ee3
yaekobbonke <yaekobbundure@gmail.com>
2023-10-23 05:27:33 +0300

    simple paginatio

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -36,10 +36,9 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """ takes two integer arguments"""
-        assert isinstance(page, int) and  page > 0
+        """takes two integer arguments"""
+        assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
-
         dataset = self.dataset()
         start_index, end_index = index_range(page, page_size)
 
