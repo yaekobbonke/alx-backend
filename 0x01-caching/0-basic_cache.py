0b907b7d5e64b451bb492240059583f3eec9df03
yaekobbonke <yaekobbundure@gmail.com>
2023-10-24 17:10:28 +0300

    basic dictionary

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -20,4 +20,4 @@ class BasicCache(BaseCaching):
 
         if key is None or key not in self.cache_data:
             return None
-        return self.cache_data.get(key)
+        return self.cache_data[key])
