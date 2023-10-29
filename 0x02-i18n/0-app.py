03948547bdd3da83272642dd42cdc8ea34922905
yaekobbonke <yaekobbundure.gmail.com>
2023-10-29 23:27:53 +0300

    app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -5,11 +5,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-
 @app.route("/")
 def index():
     """returns simple text"""
     return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run()
