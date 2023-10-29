23485c4933f65ab3ce3274b26a78e5607a940c70
yaekobbonke <yaekobbundure.gmail.com>
2023-10-29 23:23:33 +0300

    app

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Basic Flask app"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+
+@app.route("/")
+def index():
+    """returns simple text"""
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run()
