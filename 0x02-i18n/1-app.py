d8490ec4a7a55bbbf0f2043ab11a26a0e7cd76b5
yaekobbonke <yaekobbundure.gmail.com>
2023-10-30 01:19:08 +0300

    1-app

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,14 +1,27 @@
 #!/usr/bin/env python3
+
 """ Basic Babel setup"""
 
-from 0-app import app
+from flask import Flask, render_template
 from flask_babel import Babel
 
+app = Flask(__name__)
+babel = Babel(app)
+@app.route("/")
+
+
+def hello_index():
+    """returns hello world from templates"""
+    return render_template("1-index.html")
 
 class Config:
     """configure available languages"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = "UTC"
-babel = Babel(app)
+
 app.config.from_object(Config)
+
+
+if __name__ == "__main__":
+    app.run()
